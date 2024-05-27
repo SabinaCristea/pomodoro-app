ab1c2572aa5555942dbd37a0be605a0c2e284005
SabinaCristea <sabinacristea997@gmail.com>
2024-05-27 18:11:23 +0300

    Implemented the circular progress bar

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,41 +1,33 @@
-import { useEffect, useState } from "react";
+import {
+  CircularProgressbarWithChildren,
+  buildStyles,
+} from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
-function Timer({ duration }) {
-  const [progress, setProgress] = useState(0);
-
-  useEffect(() => {
-    let timer;
-    const interval = 1000;
-
-    const startTimer = () => {
-      timer = setInterval(() => {
-        setProgress((prevProgress) => {
-          if (prevProgress >= 100) {
-            clearInterval(timer);
-            return 100;
-          }
-          return prevProgress + 100 / (duration / interval);
-        });
-      }, interval);
-    };
-
-    startTimer();
-
-    return () => clearInterval(timer); // Cleanup timer on component unmount
-  }, [duration]);
-
+function Timer() {
   return (
     <div className="mb-[6.3rem]">
       <div className="outCircle flex items-center justify-center w-[41rem] h-[41rem] shrink-0 rounded-full bg-gradient shadow-custom z-0">
-        <div className="inCircle flex items-center justify-center w-[36.6rem] h-[36.6rem] rounded-full bg-dark">
-          {/* <div className="progressContainer w-[33.9rem] h-[33.9rem] shrink-0  relative"> </div> */}
-          <div className="flex flex-col items-center justify-center">
-            <div className="text-lightGray text-center text-[10rem] font-bold">
-              17:59
-            </div>
-            <div className="text-lightGray text-[1.6rem] uppercase font-bold tracking-[1.5rem] text-center place-self-center pl-[1rem]">
-              pause
-            </div>
+        <div className="inCircle relative flex items-center justify-center w-[36.6rem] h-[36.6rem] rounded-full bg-dark">
+          <div className="flex flex-col items-center justify-center absolute">
+            <CircularProgressbarWithChildren
+              value={100}
+              strokeWidth={4}
+              minValue={0}
+              maxValue={100}
+              className="w-[34rem] h-[34rem]"
+              styles={buildStyles({
+                pathColor: "#f87070",
+                trailColor: "transparent",
+              })}
+            >
+              <div className="text-lightGray text-center text-[10rem] font-bold">
+                22:22
+              </div>
+              <div className="text-lightGray text-[1.6rem] uppercase font-bold tracking-[1.5rem] text-center place-self-center pl-[1rem]">
+                pause
+              </div>
+            </CircularProgressbarWithChildren>
           </div>
         </div>
       </div>
